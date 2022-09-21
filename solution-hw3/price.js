718fdfcdca1aabcfcfffcfa76762e9cec093dfcc
rosieg45 <112334026+rosieg45@users.noreply.github.com>
2022-09-21 17:17:33 -0400

    push

--- solution-hw3/price.js
+++ solution-hw3/price.js
@@ -2,7 +2,17 @@ let basePrice = 2.49;
 let glazingPrice = 0;
 let packPrice = 0;
 
+packSizeObj1 = {size: "1", priceAdap: 1};
+packSizeObj2 = {size: "6", priceAdap: 5};
+packSizeObj3 = {size: "3", priceAdap: 3};
+packSizeObj4 = {size: "12", priceAdap: 10};
+listObj = [packSizeObj1, packSizeObj2, packSizeObj3, packSizeObj4];
+
+
+
 function glazingChange(element) {
+  console.log("in glaze change");
+
     const priceChange = element.value;
     console.log(priceChange);
     if ("Keep original" === priceChange) {
@@ -18,22 +28,32 @@ function glazingChange(element) {
   }
 
 function updatePrice() {
+  console.log("in price update");
+
   let total = document.querySelector("#next");
+  console.log("next =", total.innerHTML);
+
+  console.log("basePrice =" ,basePrice, "glazingPrice =" ,glazingPrice, "packPrice =" ,packPrice);
+
   let price = (basePrice + glazingPrice) * packPrice;
   total.innerText = "$" + price.toFixed(2);
-  console.log(total);
+  console.log("total =" ,total);
 }
 
 function packChange(element) { 
-  const pack = element.value; 
-  if ("1" === pack) {
-    packPrice = 1;
-  } else if ("3" === pack) {
-    packPrice = 3;
-  } else if ("6" === pack) {
-    packPrice = 5;
-  } else if ("12" === pack) {
-    packPrice = 10;
-  }
+  console.log("in pack change");
+
+  const pack = element.value;
+  console.log("pack size changed to =", pack);
+
+
+  listObj.forEach(function (obj) {
+    console.log("in pack change LOOP");
+    if(pack === obj.size){
+    packPrice = obj.priceAdap;
+    console.log("obj=",obj); 
+    }
+});
   updatePrice();
 }
+
