b1a672f39739aca93731143b90fc8d03567b70ab
rosieg45 <112334026+rosieg45@users.noreply.github.com>
2022-09-21 18:02:04 -0400

    push

--- solution-hw3/price.js
+++ solution-hw3/price.js
@@ -2,12 +2,49 @@ let basePrice = 2.49;
 let glazingPrice = 0;
 let packPrice = 0;
 
+// glazing dropdown
+let select = document.getElementById("glazingOptions");
+
+let glazeOp1 = document.createElement("option");
+glazeOp1.innerHTML = "Keep original";
+select.appendChild(glazeOp1);
+
+let glazeOp2 = document.createElement("option");
+glazeOp2.innerHTML = "Sugar milk";
+select.appendChild(glazeOp2);
+
+let glazeOp3 = document.createElement("option");
+glazeOp3.innerHTML = "Vanilla milk";
+select.appendChild(glazeOp3);
+
+let glazeOp4 = document.createElement("option");
+glazeOp4.innerHTML = "Double chocolate";
+select.appendChild(glazeOp4);
+
+//price dropdown
 packSizeObj1 = {size: "1", priceAdap: 1};
 packSizeObj2 = {size: "6", priceAdap: 5};
 packSizeObj3 = {size: "3", priceAdap: 3};
 packSizeObj4 = {size: "12", priceAdap: 10};
 listObj = [packSizeObj1, packSizeObj2, packSizeObj3, packSizeObj4];
 
+let price = document.getElementById("packSizes");
+
+let packSize1 = document.createElement("option");
+packSize1.innerHTML = "1";
+price.appendChild(packSize1);
+
+let packSize2 = document.createElement("option");
+packSize2.innerHTML = "3";
+price.appendChild(packSize2);
+
+let packSize3 = document.createElement("option");
+packSize3.innerHTML = "6";
+price.appendChild(packSize3);
+
+let packSize4 = document.createElement("option");
+packSize4.innerHTML = "12";
+price.appendChild(packSize4);
 
 
 function glazingChange(element) {
@@ -49,11 +86,11 @@ function packChange(element) {
 
   listObj.forEach(function (obj) {
     console.log("in pack change LOOP");
-    if(pack === obj.size){
+    if (pack === obj.size){
     packPrice = obj.priceAdap;
     console.log("obj=",obj); 
     }
-});
+  });
   updatePrice();
 }
 
