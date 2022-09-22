06d06b6694e8b60550880904449ebf027b88b4a1
rosieg45 <112334026+rosieg45@users.noreply.github.com>
2022-09-21 20:58:11 -0400

    took out glaze function

--- solution-hw3/price.js
+++ solution-hw3/price.js
@@ -2,7 +2,21 @@ let basePrice = 2.49;
 let glazingPrice = 0;
 let packPrice = 0;
 
-// glazing dropdown
+//Updating price 
+function updatePrice() {
+  console.log("in price update");
+
+  let total = document.querySelector("#next");
+  console.log("next =", total.innerHTML);
+
+  console.log("basePrice =" ,basePrice, "glazingPrice =" ,glazingPrice, "packPrice =" ,packPrice);
+
+  let price = (basePrice + glazingPrice) * packPrice;
+  total.innerText = "$" + price.toFixed(2);
+  console.log("total =" ,total);
+}
+
+//Glazing dropdown
 glazeOpObj1 = {glazingOptions: "Keep original", priceAdapt: 0};
 glazeOpObj2 = {glazingOptions: "Sugar milk", priceAdapt: 0};
 glazeOpObj3 = {glazingOptions: "Vanilla milk", priceAdapt: 0.50};
@@ -27,7 +41,7 @@ let glazeOp4 = document.createElement("option");
 glazeOp4.innerHTML = "Double chocolate";
 select.appendChild(glazeOp4);
 
-//price dropdown
+//Price dropdown
 packSizeObj1 = {size: "1", priceAdap: 1};
 packSizeObj2 = {size: "6", priceAdap: 5};
 packSizeObj3 = {size: "3", priceAdap: 3};
@@ -52,21 +66,13 @@ let packSize4 = document.createElement("option");
 packSize4.innerHTML = "12";
 price.appendChild(packSize4);
 
-
+//Changing glaze
 function glazingChange(element) {
   console.log("in glaze change");
 
     const priceChange = element.value;
     console.log(priceChange);
-    // if ("Keep original" === priceChange) {
-    //   glazingPrice = 0;
-    // } else if ("Vanilla milk" === priceChange) {
-    //   glazingPrice = .50;
-    // } else if ("Sugar milk" === priceChange) {
-    //   glazingPrice = 0;
-    // } else if ("Double chocolate" === priceChange) {
-    //   glazingPrice = 1.50;
-    // }
+
     listGlazeObj.forEach(function (obj) {
       console.log("in pack change LOOP");
       if (glaze === obj.glazingOptions){
@@ -74,22 +80,11 @@ function glazingChange(element) {
       console.log("obj=",obj); 
       }
     });
-    updatePrice();
-  }
-
-function updatePrice() {
-  console.log("in price update");
-
-  let total = document.querySelector("#next");
-  console.log("next =", total.innerHTML);
-
-  console.log("basePrice =" ,basePrice, "glazingPrice =" ,glazingPrice, "packPrice =" ,packPrice);
-
-  let price = (basePrice + glazingPrice) * packPrice;
-  total.innerText = "$" + price.toFixed(2);
-  console.log("total =" ,total);
+  updatePrice();
 }
 
+
+//Changing price
 function packChange(element) { 
   console.log("in pack change");
 
